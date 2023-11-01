c3f9a1edac5a4e10714d4b78815fcb20021278cd
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-11-01 11:19:34 -0300

    feat: add placeholder text to inputs on UserForm

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -19,11 +19,11 @@ function UserForm({ onAddUser }) {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='name'>Name</label>
-        <input id='name' value={name} onChange={(e) => setName(e.target.value)} />
+        <input id='name' value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter your name'/>
       </div>
       <div>
         <label htmlFor='email'>Email</label>
-        <input id='email'value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input id='email'value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter your email'/>
       </div>
       <button>Add User</button>
     </form>
