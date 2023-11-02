9dfcbb937042bc5338281219a0aa6f854e000f53
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-11-02 12:47:44 -0300

    test: refactor test to make use of a helper function in order to render the component inside UserList test

--- src/UserList.test.tsx
+++ src/UserList.test.tsx
@@ -1,45 +1,39 @@
-import { render, screen, within } from '@testing-library/react';
-import UserList from './UserList';
+import { render, screen, within } from "@testing-library/react";
+import UserList from "./UserList";
 
-describe('UserList', () => {
-    it('should render one row per user', () => {
-        const users = [
-            {
-                name: 'Robin', 
-                email: 'robin@robin.com'
-            }, 
-            {
-                name: 'Batman', 
-                email: 'batman@robin.com'
-            }
-        ]
-        render(<UserList users={users} />)
+describe("UserList", () => {
+  const renderComponent = () => {
+    const users = [
+      {
+        name: "Robin",
+        email: "robin@robin.com",
+      },
+      {
+        name: "Batman",
+        email: "batman@robin.com",
+      },
+    ];
 
-       const rows = within(screen.getByTestId('users')).getAllByRole('row')
+    render(<UserList users={users} />);
 
-       expect(rows).toHaveLength(2)
-        
-    })
+    return { users };
+  };
 
-    it('should render the name and email of each user', () => {
-        const users = [
-            {
-                name: 'Robin', 
-                email: 'robin@robin.com' 
-            }, 
-            {
-                name: 'Batman', 
-                email: 'batman@robin.com'
-            }
-        ]
+  it("should render one row per user", () => {
+    renderComponent();
 
-        render(<UserList users={users} />)
+    const rows = within(screen.getByTestId("users")).getAllByRole("row");
+    expect(rows).toHaveLength(2);
+  });
 
-        for (let user of users) {
-            const name = screen.getByRole('cell', {name: user.name})
-            const email = screen.getByRole('cell', {name: user.email})
-            expect(name).toBeInTheDocument();
-            expect(email).toBeInTheDocument();
-        }
-    })
-})
+  it("should render the name and email of each user", () => {
+    const { users } = renderComponent();
+
+    for (let user of users) {
+      const name = screen.getByRole("cell", { name: user.name });
+      const email = screen.getByRole("cell", { name: user.email });
+      expect(name).toBeInTheDocument();
+      expect(email).toBeInTheDocument();
+    }
+  });
+});
