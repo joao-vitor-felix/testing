aa76586eb830ec027a69bf4e1c3745351af7e576
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-11-02 12:49:39 -0300

    style: change indentation inside UserForm

--- src/UserForm.tsx
+++ src/UserForm.tsx
@@ -1,33 +1,43 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState } from "react";
 
 type UserFormProps = {
-  onAddUser: (users: {name: string, email: string}) => void;
-}
+  onAddUser: (users: { name: string; email: string }) => void;
+};
 
 function UserForm({ onAddUser }: UserFormProps) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
 
     const user = {
       name,
-      email
+      email,
     };
 
-   return onAddUser(user);
+    return onAddUser(user);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor='name'>Name</label>
-        <input id='name' value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter your name' />
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter your name"
+        />
       </div>
       <div>
-        <label htmlFor='email'>Email</label>
-        <input id='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter your email' />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email"
+        />
       </div>
       <button>Add User</button>
     </form>
